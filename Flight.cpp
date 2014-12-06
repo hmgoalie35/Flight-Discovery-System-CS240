fbdd69608ceed7f280a8adefd0ec4260d388ac3c
Harris Pittinsky <hpittin1@binghamton.edu>
2014-12-06 18:11:14 -0500

    add visited boolean and getters/setters. Add getter for flight duration and departure city

--- Flight.cpp
+++ Flight.cpp
@@ -13,6 +13,7 @@ Flight::Flight(string the_departure, string the_destination, Time the_departure_
     departure_time = the_departure_time;
     arrival_time = the_arrival_time;
     flight_duration = arrival_time - departure_time;
+    visited = false;
 }
 //Prints out a Flight object. Formatted as Departure, Destination, Departure Time, Arrival Time, Cost, Flight Duration.
 ostream& operator<< (ostream& out, const Flight& flight) {
@@ -21,19 +22,39 @@ ostream& operator<< (ostream& out, const Flight& flight) {
 }
 
 //checks to see if one flight has a shorter duration than the other
-bool Flight::operator<(const Flight& flight) {
-    return (flight_duration < flight.flight_duration);
+bool Flight::operator<(const Flight& flight) const{
+    return (this->flight_duration < flight.flight_duration);
 }
 
 //checks to see if one flight has a longer duration than the other
-bool Flight::operator>(const Flight& flight) {
-    return (flight_duration > flight.flight_duration);
+bool Flight::operator>(const Flight& flight) const{
+    return (this->flight_duration > flight.flight_duration);
 
 }
 
 //checks to see if both flights have the same duration
-bool Flight::operator==(const Flight& flight) {
+bool Flight::operator==(const Flight& flight) const{
     return (flight_duration == flight.flight_duration);
+}
+
+//return the flight duration
+TimeLength Flight::get_flight_duration(){
+    return flight_duration;
+}
+
+//return the departure city.
+string Flight::get_departure_city(){
+    return departure;
+}
+
+//mark this flight as visited or not visited.
+void Flight::set_visited(bool new_value){
+    visited = new_value;
+}
+
+//check if the flight was already visited.
+bool Flight::was_visited(){
+    return visited;
 }
 
 //destructor
